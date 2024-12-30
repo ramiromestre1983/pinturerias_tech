69e80b91b7ec8544665ad1286dc5d1531cb9a33d
ramiromestre1983 <126579222+ramiromestre1983@users.noreply.github.com>
2024-12-30 00:18:04 -0300

    Añadir archivos mediante carga
    se agregaron formularios

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+function alerta()
+{
+    alert("hola mundo");
+}
+
+
+let miboton_contacto=document.getElementById("boton_contacto");
+miboton_contacto.addEventListener("click", function() { alert("sdsdsds"); 
+    // 'this' se refiere al elemento que disparó el evento 
+    });
